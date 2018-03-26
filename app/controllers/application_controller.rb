d3255a70c4d2f6084d636331e4917bbb8681e274
xiaowei <shenzhoudance@gmail.com>
2018-03-26 16:31:01 +0800

    add paperclip for image uploadomg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
+   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+ 	protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:account_update) << :name
-  end
-end
+ 	def configure_permitted_parameters
+ 	  devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+ 	  devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+ 	end
+ end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,6 +45,6 @@ end
   end
 
   def post_params
-    params.require(:post).permit(:title, :link, :description)
+    params.require(:post).permit(:title, :link, :description, :image)
   end
 end
